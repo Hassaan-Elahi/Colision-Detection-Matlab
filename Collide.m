f6d0bc5c987f64e91522ba01146e37e01b9786ec
Kamaal <kamal1subhani@gmail.com>
2018-12-06 14:14:06 +0500

    initial work for vision project

--- Collide.m
+++ Collide.m
@@ -1,1 +1,41 @@
-hello my name is kamal subhani. theta
+input=VideoReader('vid3.mp4');
+frames = VideoReader('vid3.mp4');
+
+%No of frames in the video
+frames = ceil(frames.FrameRate*frames.Duration);
+
+i=0;
+
+[y, Fs] = audioread('beep.mp3');
+        player = audioplayer(y, Fs);
+        
+        [y1, Fs1] = audioread('beep1.mp3');
+        player1 = audioplayer(y1, Fs1);
+
+while(i<frames-1)
+    img=readFrame(input);
+  
+    thres=graythresh(img);
+    i2=~(im2bw(img,thres));
+    
+    i2 = bwareafilt(i2, 1);
+    
+    se = offsetstrel('ball',5,5);
+    i2 = imerode(i2,se);
+
+    
+    Total_White_Pixels = nnz(i2);
+    
+    display(Total_White_Pixels);
+    
+    if(Total_White_Pixels>100000)
+        play(player);
+    end
+    if(Total_White_Pixels>200000)
+        play(player1);
+    end
+    
+
+     imshow(i2);
+    i=i+1;
+end
